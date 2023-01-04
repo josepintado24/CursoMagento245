dcc9bf404073c674833ed5fe30310515ac6dcd2f
Jose Luis Pintado VAsquez <josepintado@gmail.com>
2023-01-04 01:05:08 -0500

    System.xml

--- app/code/Margento/ModuloBasico/Block/Margento.php
+++ app/code/Margento/ModuloBasico/Block/Margento.php
@@ -1,15 +1,26 @@
 <?php
 
 namespace Margento\ModuloBasico\Block;
+
 use Magento\Framework\View\Element\Template;
 
 class Margento extends Template
 {
+
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        array $data=[]
-    )
-    {
+        public \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        array $data = []
+    ) {
         parent::__construct($context, $data);
+    }
+
+    public function getJjgay()
+    {
+       
+            return $this->scopeConfig->getValue(
+                'jlsection/jlgroup/title',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
     }
 }
