af1a40edb1126d7c31587d9cb49bd340d611c992
Jose Luis Pintado VAsquez <josepintado@gmail.com>
2023-01-18 14:25:35 -0500

    Creación de tema | integrar variables y fuentes | Grunt config

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -19,6 +19,18 @@
  *
  */
 module.exports = {
+    Heve: {
+        area: 'frontend',
+        name: 'Magento/Heve',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
     blank: {
         area: 'frontend',
         name: 'Magento/blank',
